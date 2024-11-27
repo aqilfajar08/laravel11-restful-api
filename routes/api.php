46d6e22598ec2e8d00b777997ac125a10745ae15
aqilfajar08 <qilzmi08@gmail.com>
2024-11-27 21:23:58 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
